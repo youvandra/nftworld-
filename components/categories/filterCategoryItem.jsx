c5e663df8d1e5a67684b5d1478e89f78b5517b84
anasdeyra <anasdoura607@gmail.com>
2022-11-26 15:28:54 +0100

    explore collections

--- components/categories/filterCategoryItem.jsx
+++ components/categories/filterCategoryItem.jsx
@@ -1,24 +1,44 @@
-import React, { useEffect } from 'react';
-import { trendingCategoryData } from '../../data/categories_data';
-import Collection_category_filter from '../collectrions/collection_category_filter';
-import CategoryItem from './categoryItem';
-import { useDispatch } from 'react-redux';
-import { updateTrendingCategoryItemData } from '../../redux/counterSlice';
+import React, { useEffect, useState } from "react";
+import { trendingCategoryData } from "../../data/categories_data";
+import Collection_category_filter from "../collectrions/collection_category_filter";
+import { useDispatch } from "react-redux";
+import { updateTrendingCategoryItemData } from "../../redux/counterSlice";
+import OwnedItem from "./ownedItem";
+import { useNFTs, useProgram } from "@thirdweb-dev/react/solana";
+import { useRouter } from "next/router";
 
 const FilterCategoryItem = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(updateTrendingCategoryItemData(trendingCategoryData.slice(0, 8)));
-	}, []);
+  const [nfts, setNFTs] = useState(trendingCategoryData.slice(0, 8));
 
-	return (
-		<div>
-			{/* <!-- Filter --> */}
-			<Collection_category_filter />
-			<CategoryItem />
-		</div>
-	);
+  const router = useRouter();
+  const address = router.query.collection;
+
+  const { program } = useProgram(address);
+
+  const { data } = useNFTs(program);
+
+  useEffect(() => {
+    if (!data) return;
+
+    const formatedNFTs = data.map(
+      ({ metadata: { id, image, name: title } }) => ({ id, image, title })
+    );
+    setNFTs(formatedNFTs);
+  }, [data]);
+
+  useEffect(() => {
+    dispatch(updateTrendingCategoryItemData(nfts));
+  }, [nfts]);
+
+  return (
+    <div>
+      {/* <!-- Filter --> */}
+      <Collection_category_filter />
+      <OwnedItem />
+    </div>
+  );
 };
 
 export default FilterCategoryItem;
