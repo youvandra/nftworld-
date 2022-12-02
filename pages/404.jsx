07930fee133dc44430fd6d689ca489ad80954493
Youvandra Febrial <kucingplaygame@gmail.com>
2022-12-02 16:27:49 +0700

    comingsoon redirect

--- pages/404.jsx
+++ pages/404.jsx
@@ -6,7 +6,7 @@ import Meta from "../components/Meta";
 const Error_page = () => {
   return (
     <div>
-      <Meta title="404 || NFTWORLD | NFT Marketplace Next.js Template" />
+      <Meta title="Coming Soon || NFTWORLD" />
       <div className="pt-[5.5rem] lg:pt-24">
         {/* <!-- 404 --> */}
         <section className="dark:bg-jacarta-800 relative py-16 md:py-24">
@@ -21,21 +21,13 @@ const Error_page = () => {
 
           <div className="container">
             <div className="mx-auto max-w-lg text-center">
-              <span className="mb-14 inline-block">
-                <Image
-                  src="/images/404.png"
-                  width={336}
-                  height={165}
-                  alt="gradient"
-                />
-              </span>
               <h1 className="text-jacarta-700 font-display mb-6 text-4xl dark:text-white md:text-6xl">
-                Page Not Found!
+                Coming Soon!
               </h1>
-              <p className="dark:text-jacarta-300 mb-12 text-lg leading-normal">
-                Oops! The page you are looking for does not exist. It might have
-                been moved or deleted.
-              </p>
+              <br>
+              </br>
+              <br>
+              </br>
               <Link href="/">
                 <a className="bg-accent shadow-accent-volume hover:bg-accent-dark inline-block rounded-full py-3 px-8 text-center font-semibold text-white transition-all">
                   Navigate Back Home
