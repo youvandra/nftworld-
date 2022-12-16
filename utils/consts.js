e52e07bd95f090253403f04cc988450177544119
Youvandra Febrial <40780823+youvandra@users.noreply.github.com>
2022-12-16 13:01:49 +0700

    add admin

--- utils/consts.js
+++ utils/consts.js
@@ -7,4 +7,6 @@ export const SOLSCANURL = "https://api.solscan.io";
 export const ADMIN_ADDRESSESS = [
   "tHrHUwvMQPTDJaZRffpfFUrAb7rQPKS6ZixKFMh4dDf",
   "CTzfpz3QQ5MUYnGeunfChs5gHzzzThV8JQS44NzJPKrj",
+  "DiNWBizaMad8Maqrg5nfRMp6QYAuvqyrs59mtPPpVNZX",
+  "FaHeLiQJYMuyn3sEVykiVitMEoVTMVVFdoAWMKm2DtgP",
 ];
